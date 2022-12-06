db260b7713cc8f8bb375c46d2c69e635ff5b03ce
Krenz.Eric <e.krenz@fz-juelich.de>
2022-12-06 08:55:40 +0100

    add day 6

---
+++ 06/a.py
@@ -0,0 +1,38 @@
+def main():
+    # gets data from the a.txt file in the same directory
+    file = open("a.txt", "r")
+
+    signal = file.readline()
+
+    letters = []
+
+    # reads the data from the file by line
+    for letter in signal:
+        letters.append(letter)
+
+    # print(f"Letters: {len(letters)}")
+
+    for n in range(3, len(letters)):
+
+        last_four_letters = letters[n - 3 : n + 1]
+
+        # see if all four letters are different
+        if (
+            last_four_letters[0] != last_four_letters[1]
+            and last_four_letters[0] != last_four_letters[2]
+            and last_four_letters[0] != last_four_letters[3]
+            and last_four_letters[1] != last_four_letters[2]
+            and last_four_letters[1] != last_four_letters[3]
+            and last_four_letters[2] != last_four_letters[3]
+        ):
+            print(last_four_letters)
+            # print final location, account for zero indexing of array
+            print(f"Location: {n + 1}")
+            break
+
+    # close the file
+    file.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 06/b.py
@@ -0,0 +1,46 @@
+def main():
+    # gets data from the a.txt file in the same directory
+    file = open("a.txt", "r")
+
+    signal = file.readline()
+
+    letters = []
+
+    # reads the data from the file by line
+    for letter in signal:
+        letters.append(letter)
+
+    # print(f"Letters: {len(letters)}")
+
+    for n in range(13, len(letters)):
+
+        last_fourteen_letters = str(letters[n - 13 : n + 1])
+
+        # see if all four letters are different
+        if (
+            last_fourteen_letters.count(letters[n - 13]) == 1
+            and last_fourteen_letters.count(letters[n - 12]) == 1
+            and last_fourteen_letters.count(letters[n - 11]) == 1
+            and last_fourteen_letters.count(letters[n - 10]) == 1
+            and last_fourteen_letters.count(letters[n - 9]) == 1
+            and last_fourteen_letters.count(letters[n - 8]) == 1
+            and last_fourteen_letters.count(letters[n - 7]) == 1
+            and last_fourteen_letters.count(letters[n - 6]) == 1
+            and last_fourteen_letters.count(letters[n - 5]) == 1
+            and last_fourteen_letters.count(letters[n - 4]) == 1
+            and last_fourteen_letters.count(letters[n - 3]) == 1
+            and last_fourteen_letters.count(letters[n - 2]) == 1
+            and last_fourteen_letters.count(letters[n - 1]) == 1
+            and last_fourteen_letters.count(letters[n]) == 1
+        ):
+            print(last_fourteen_letters)
+            # print final location, account for zero indexing of array
+            print(f"Location: {n + 1}")
+            break
+
+    # close the file
+    file.close()
+
+
+if __name__ == "__main__":
+    main()
